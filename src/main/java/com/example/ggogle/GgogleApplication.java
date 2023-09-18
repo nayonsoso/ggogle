7996ef4e0d7e7d1d3c94fd508b60a5199ca2c53c
nayonsoso <panda0329@naver.com>
2023-09-18 16:12:47 +0900

    first commit

---
+++ src/main/java/com/example/ggogle/GgogleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ggogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgogleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgogleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ggogle/GgogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ggogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgogleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
